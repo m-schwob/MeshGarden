4b91eb0c5da0e715dd02f05cf3dd4481731f2e9a
m-schwob <menashe54@gmail.com>
2022-05-09 20:43:51 +0300

    reformat mesh node class

--- src/mesh_node.cpp
+++ src/mesh_node.cpp
@@ -4,8 +4,8 @@
 
   This is a simple example that uses the painlessMesh library: https://github.com/gmag11/painlessMesh/blob/master/examples/basic/basic.ino
 //*/
-#include "painlessMesh.h"
-#include "string.h"
+#include <painlessMesh.h>
+#include <string.h>
 #include <iostream>
 #include <list>
 
@@ -15,76 +15,43 @@
 
 class Mesh_Node
 {
-private:
-	Scheduler userScheduler; // to control your personal task
-	painlessMesh mesh;
-	int counter;
-	std::list<int> counterList;
-	Task taskSendMessage;
+	private:
+		painlessMesh mesh;
+		Scheduler userScheduler; // to control your personal task
+		Task taskSendMessage;
+		int counter;
+		std::list<int> counterList;
 
-	friend void receivedCallback(uint32_t from, String &msg);
-	friend void newConnectionCallback(uint32_t nodeId);
-	friend void changedConnectionCallback();
-	friend void nodeTimeAdjustedCallback(int32_t offset);
-	friend void sendMessage();
+		friend void receivedCallback(uint32_t from, String &msg);
+		friend void newConnectionCallback(uint32_t nodeId);
+		friend void changedConnectionCallback();
+		friend void nodeTimeAdjustedCallback(int32_t offset);
+		friend void sendMessage();
 
-public:
-	Mesh_Node() : counter(0), taskSendMessage(TASK_SECOND*20 , TASK_FOREVER, &sendMessage) {
-		setup_node();
-	}
-	// User stub
-	void mapNetwork();
-
-	
-	// Task mapAccessPoints( TASK_SECOND * 20, TASK_FOREVER , &mapNetwork);
-	// TODO figgure out if there is need to split.
-	void TaskManager()
-	{
-		userScheduler.addTask(taskSendMessage);
-		taskSendMessage.enable();
-	}
-	
-
-	void setup_node()
-	{
-		Serial.begin(115200);
-		// mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-		mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
-		mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
-		mesh.onReceive(&receivedCallback);
-		mesh.onNewConnection(&newConnectionCallback);
-		mesh.onChangedConnections(&changedConnectionCallback);
-		mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-		mesh.onDroppedConnection([](uint32_t nodeId)
-								 {
-    // Do something with the event
-    Serial.println(String(nodeId)); });
-		TaskManager();
-	}
-
-	void loop_node()
-	{
-		// it will run the user scheduler as well
-		mesh.update();
-	}
+	public:
+		Mesh_Node();
+		void update();
 };
+
+static Mesh_Node *node = NULL;
 
 // for Now the message simply contains the nodeId and a counter
 void sendMessage()
 {
-	String msg;
-	msg += mesh.getNodeId();
-	msg += " ";
-	msg += mesh.counter;
-	counter++;
-	Serial.println(msg);
-	mesh.sendBroadcast(msg);
+	// String msg;
+	// msg += mesh.getNodeId();
+	// msg += " ";
+	// msg += counter;
+	// counter++;
+	// Serial.println(msg);
+	// mesh.sendBroadcast(msg);
 }
 
 void receivedCallback(uint32_t from, String &msg)
 {
 	Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
+
 void newConnectionCallback(uint32_t nodeId)
 {
 	Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
@@ -97,6 +64,24 @@ void changedConnectionCallback()
 
 void nodeTimeAdjustedCallback(int32_t offset)
 {
-	Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+	Serial.printf("Adjusted time %u. Offset = %d\n", node->mesh.getNodeTime(), offset);
 }
 
+
+Mesh_Node::Mesh_Node() : taskSendMessage(TASK_SECOND * 20, TASK_FOREVER, &sendMessage), counter(0){
+	Serial.begin(115200);
+	// mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+	mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
+	mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
+	mesh.onReceive(&receivedCallback);
+	mesh.onNewConnection(&newConnectionCallback);
+	mesh.onChangedConnections(&changedConnectionCallback);
+	mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+	mesh.onDroppedConnection([](uint32_t nodeId){ Serial.println(String(nodeId)); });
+	taskSendMessage.enable();
+	node = this;
+}
+
+void Mesh_Node::update(){
+	mesh.update();
+}
