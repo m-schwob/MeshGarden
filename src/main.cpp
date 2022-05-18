c8f04c2403da48f1a9dcec0f0f0573292f4cf591
m-schwob <menashe54@gmail.com>
2022-05-18 23:10:51 +0300

    add init example for dummy sensor

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,9 @@
     #include "dummy_sensor.h"
     MeshNode* mesh = NULL;
     SoilMoisureSensorGroveV1* soil_moisture = NULL;
-    DummySensor* dummy_sensor = NULL;
+    DummySensor* dummy_sensor1 = NULL;
+    DummySensor* dummy_sensor2 = NULL;
+    DummySensor* dummy_sensor3 = NULL;
 #endif
 
 
@@ -30,16 +32,37 @@ void setup(void)
         mesh = new MeshBridge();
     #else
         mesh = new MeshNode();
-        soil_moisture = new SoilMoisureSensorGroveV1(1,0);
-        dummy_sensor = new DummySensor(0, "Temperature", "Temperature", "Celsius");
+        soil_moisture = new SoilMoisureSensorGroveV1(0,0);
+
+        // init single measurement type dummy sensor 
+        dummy_sensor1 = new DummySensor(1, "Temperature", "Temperature", "Celsius");
+
+        // init multiple measurement types dummy sensor using {...} methode (or pointer to array)
+        // NOTE!!! this init method made for convenience but im not sure it works XD. be careful.
+        dummy_sensor2 = new DummySensor(2, {"Temperature", "Humidity"}, {"Temperature", "Humidity"}, {"Celsius", "%"});
+
+        // init multiple measurement types dummy sensor directly using std::vector<String>. this is more robust init methode
+        dummy_sensor3 = new DummySensor(3, std::vector<String>({"Temperature", "Humidity"}), std::vector<String>({"Temperature", "Humidity"}), std::vector<String>({"Celsius", "%"}));
+
+
         soil_moisture->enable(true);
-        dummy_sensor->enable(true);
+        dummy_sensor1->enable(true);
+        dummy_sensor2->enable(true);
+        dummy_sensor3->enable(true);
+
+
         Serial.println("adding tasks");
         mesh->add_measurement(soil_moisture->get_measure_callback(), 2);
         mesh->add_send_values(soil_moisture->get_values_callback(), 4);
-        mesh->add_measurement(dummy_sensor->get_measure_callback(), 2);
-        mesh->add_send_values(dummy_sensor->get_values_callback(), 4);
 
+        mesh->add_measurement(dummy_sensor1->get_measure_callback(), 2);
+        mesh->add_send_values(dummy_sensor1->get_values_callback(), 4);
+
+        mesh->add_measurement(dummy_sensor2->get_measure_callback(), 2);
+        mesh->add_send_values(dummy_sensor2->get_values_callback(), 4);
+
+        mesh->add_measurement(dummy_sensor3->get_measure_callback(), 2);
+        mesh->add_send_values(dummy_sensor3->get_values_callback(), 4);
     #endif
     Serial.println("setup done");
 }
