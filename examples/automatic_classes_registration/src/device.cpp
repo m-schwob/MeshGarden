4026cda3f45db89e610b2d95dcc2777c526fb940
m-schwob <menashe54@gmail.com>
2022-06-05 22:19:32 +0300

    refactor device and sensor classes for the new design

--- examples/automatic_classes_registration/src/device.cpp
+++ examples/automatic_classes_registration/src/device.cpp
@@ -1,33 +1,53 @@
 #include "device.h"
 
 
-Device::Device(int id, String device_type, String hardware_info, uint8_t data_pins, uint8_t power_pin)
-        : Device(id, std::vector<String>({device_type}), hardware_info, data_pins, power_pin){}
-
-Device::Device(int id, String device_type[], String hardware_info, uint8_t data_pins, uint8_t power_pin)
-        : Device(id, std::vector<String>(device_type, arr_end(device_type)), hardware_info, data_pins, power_pin){}
-
-Device::Device(int id, std::vector<String> device_type, String hardware_info, uint8_t data_pins, uint8_t power_pin) 
-        : ID(id), DEVICE_TYPE(device_type), HARDWARE_INFO(hardware_info), DATA_PINS(data_pins), POWER_PIN(power_pin) {
-    if(POWER_PIN != -1){
-        pinMode(POWER_PIN, OUTPUT);
-        digitalWrite(POWER_PIN, LOW);
+Device::Device(DEVICE_CONSTRUCTOR_ARGUMENTS)
+    : DEVICE_ID(device_id), DEVICE_TYPE(envelop[DEVICE_TYPE_KEY].as<String>()), 
+        HARDWARE_INFO(hardware_info), PINOUT(pinout), POWER_PIN_CONTROL(pinout.containsKey(POWER_PIN)? true:false)
+{
+    if (POWER_PIN_CONTROL)
+    {
+        pinMode(POWER_PIN_CONTROL, OUTPUT);
+        digitalWrite(POWER_PIN_CONTROL, LOW);
     }
 }
 
+DynamicJsonDocument Device::create_envelop(String device_type){
+    DynamicJsonDocument doc(JSON_OBJECT_SIZE(1) + JSON_STRING_SIZE(10));
+    doc[DEVICE_TYPE_KEY] = device_type;
+    if(doc.overflowed())
+        Serial.println("ERROR: device_type string overflow"); // think how to handle this case. exit? just log?
+    doc.shrinkToFit();
+    return doc;
+}
+
+// Device::Device(int id, String device_type, String hardware_info, uint8_t data_pins, uint8_t power_pin)
+//         : Device(id, std::vector<String>({device_type}), hardware_info, DynamicJsonDocument pinout){}
+
+// Device::Device(int id, String device_type[], String hardware_info, uint8_t data_pins, uint8_t power_pin)
+//         : Device(id, std::vector<String>(device_type, arr_end(device_type)), hardware_info, DynamicJsonDocument pinout){}
+
+// Device::Device(int id, std::vector<String> device_type, String hardware_info, DynamicJsonDocument pinout) 
+//         : ID(id), DEVICE_TYPE(device_type), HARDWARE_INFO(hardware_info), PINOUT(pinout) {
+//     if(POWER_PIN_CONTROL != -1){
+//         pinMode(POWER_PIN_CONTROL, OUTPUT);
+//         digitalWrite(POWER_PIN_CONTROL, LOW);
+//     }
+// }
+
 void Device::power_on(){
-    if(POWER_PIN != -1)
-        digitalWrite(POWER_PIN, HIGH);
+    if(POWER_PIN_CONTROL)
+        digitalWrite(POWER_PIN_CONTROL, HIGH);
 }
 void Device::power_off(){
-    if(POWER_PIN != -1)
-        digitalWrite(POWER_PIN, LOW);
+    if(POWER_PIN_CONTROL)
+        digitalWrite(POWER_PIN_CONTROL, LOW);
 }
 
 // by default enabling the device does not power it on. 
 void Device::enable(bool _power_on){
     enabled = true;
-    if(_power_on && POWER_PIN != -1)
+    if(_power_on && POWER_PIN_CONTROL)
         power_on();
 }
 
--- examples/automatic_classes_registration/src/sensor.cpp
+++ examples/automatic_classes_registration/src/sensor.cpp
@@ -1,20 +1,35 @@
 #include "sensor.h"
 
+Sensor::Sensor(DEVICE_CONSTRUCTOR_ARGUMENTS)
+    : Device(device_id, hardware_info, pinout, create_envelop(DEVICE_TYPE_SENSOR)),
+      MEASUREMENTS_TYPE(from_envelop(envelop, MEASUREMENT_TYPE_KEY)), UNITS(from_envelop(envelop, UNITS_KEY)) {}
 
-Sensor::Sensor(int id, String sensor_type, String hardware_info, uint8_t data_pins, String measurement_type, String units, uint8_t power_pin)
-        : Sensor(id, std::vector<String>({sensor_type}), hardware_info, data_pins, std::vector<String>({measurement_type}), std::vector<String>({units}), power_pin) {}
+std::vector<String> Sensor::from_envelop(DynamicJsonDocument envelop, String key)
+{
+    JsonArray arr = envelop[key];
+    std::vector<String> vec(arr.size());
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        vec[i] = arr[i].as<String>();
+    }
+    return vec;
+}
 
-// TODO validate inputs sizes
-Sensor::Sensor(int id, String sensor_type[], String hardware_info, uint8_t data_pins, String measurement_type[], String units[], uint8_t power_pin)
-        : Sensor(id, std::vector<String>(sensor_type, arr_end(sensor_type)), hardware_info, data_pins, std::vector<String>(measurement_type, arr_end(measurement_type)), 
-            std::vector<String>(units, arr_end(units)), power_pin) {}
+// Sensor::Sensor(int id, String sensor_type, String hardware_info, uint8_t data_pins, String measurement_type, String units, uint8_t power_pin)
+//         : Sensor(id, std::vector<String>({sensor_type}), hardware_info, data_pins, std::vector<String>({measurement_type}), std::vector<String>({units}), power_pin) {}
 
-// TODO validate inputs sizes
-Sensor::Sensor(int id, std::vector<String> sensor_type, String hardware_info, uint8_t data_pins, std::vector<String> measurements_type, std::vector<String> units, uint8_t power_pin)
-    : Device(id, sensor_type, hardware_info, data_pins, power_pin), MEASUREMENTS_TYPE(measurements_type), UNITS(units) {}
+// // TODO validate inputs sizes
+// Sensor::Sensor(int id, String sensor_type[], String hardware_info, uint8_t data_pins, String measurement_type[], String units[], uint8_t power_pin)
+//         : Sensor(id, std::vector<String>(sensor_type, arr_end(sensor_type)), hardware_info, data_pins, std::vector<String>(measurement_type, arr_end(measurement_type)),
+//             std::vector<String>(units, arr_end(units)), power_pin) {}
 
-measure_callback_t Sensor::get_measure_callback(){
-    return [this](){measure();};
+// // TODO validate inputs sizes
+// Sensor::Sensor(int id, std::vector<String> sensor_type, String hardware_info, uint8_t data_pins, std::vector<String> measurements_type, std::vector<String> units, uint8_t power_pin)
+//     : Device(id, _DEVICE_TYPE, hardware_info, data_pins, power_pin), MEASUREMENTS_TYPE(measurements_type), UNITS(units) {}
+
+measure_callback_t Sensor::get_measure_callback()
+{
+    return [this]() -> Measurements { return measure(); };
 }
 
 // get_values_callback_t Sensor::get_values_callback(){
