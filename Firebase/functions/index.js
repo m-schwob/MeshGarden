70831dddf9f6953b8fd395ab7aa8625f14355231
m-schwob <menashe54@gmail.com>
2022-06-23 22:19:05 +0300

    onNodeWrite works in emulator and  server using helper functions

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -2,19 +2,31 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+
 exports.onNodeWrite = functions.firestore.document('/Nodes/{node_id}').onWrite(async (change, context) => {
     const node_id = context.params.node_id;
 
+    await update_changes_collection(change, node_id);
+    await update_measurements_collection(change, node_id);
+
+
+});
+
+async function update_changes_collection(change, node_id) {
     if (change.after.exists) {
         const data = change.after.data();
         const doc = admin.firestore().collection('Changes').doc(node_id);
-        
         doc.set({ 'config': JSON.stringify(data) });
+        functions.logger.log("node " + node_id + " changed");
     } else {
-        functions.logger.log("Node " + node_id + "has deleted");
+        functions.logger.log("node " + node_id + " deleted");
     }
+}
 
-    const doc = admin.firestore().collection('Changes').doc('10');
-    const config = await doc.get(); 
+async function update_measurements_collection(change, node_id) {
+    const doc = admin.firestore().collection('Changes').doc(node_id);
+    const config = await doc.get();
     console.log(config.data());
-});
+}
+
+
