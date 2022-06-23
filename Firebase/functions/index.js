65dad9c374831f5ce65128cf944a228ce910c6e8
m-schwob <menashe54@gmail.com>
2022-06-23 19:46:28 +0300

    onNodeWrite works. set and get works on server.

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,9 +1,20 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.onNodeWrite = functions.firestore.document('/Nodes/{node_id}').onWrite(async (change, context) => {
+    const node_id = context.params.node_id;
+
+    if (change.after.exists) {
+        const data = change.after.data();
+        const doc = admin.firestore().collection('Changes').doc(node_id);
+        
+        doc.set({ 'config': JSON.stringify(data) });
+    } else {
+        functions.logger.log("Node " + node_id + "has deleted");
+    }
+
+    const doc = admin.firestore().collection('Changes').doc('10');
+    const config = await doc.get(); 
+    console.log(config.data());
+});
