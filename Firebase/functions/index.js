e366ae840814b0e488d85216ea85db30fffac396
m-schwob <menashe54@gmail.com>
2022-06-24 01:41:07 +0300

    add back json converting functions. remove old index backup files

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -97,3 +97,58 @@ async function update_measurements_collection(change, node_id) {
 //     }
 //     return [];
 // }
+
+/*jsonToDocumenton and documentToJson are based on https://stackoverflow.com/questions/62246410/how-to-convert-a-firestore-document-to-plain-json-and-vice-versa*/
+// function jsonToDocument(value) {
+//     if (value == null) {
+//         return { "nullValue": null };
+//     } else if (!isNaN(value)) {
+//         if (value.toString().indexOf(".") != -1) {
+//             return { "doubleValue": value };
+//         } else {
+//             return { "integerValue": value };
+//         }
+//     } else if (value === "true" || value === "false" || typeof value == "boolean") {
+//         return { "booleanValue": value };
+//     } else if (Date.parse(value)) {
+//         return { "timestampValue": value };
+//     } else if (typeof value == "string") {
+//         return { "stringValue": value };
+//     } else if (value && value.constructor === Array) {
+//         return { "arrayValue": { values: value.map((v) => jsonToDocument(v)) } };
+//     } else if (typeof value === "object") {
+//         const obj = {};
+//         for (const o in value) {
+//             obj[o] = jsonToDocument(value[o]);
+//         }
+//         return { "mapValue": { fields: obj } };
+//     }
+// }
+
+// /* */
+// function documentToJson(fields) {
+//     const result = {};
+//     for (const f in fields) {
+//         const key = f;
+//         const value = fields[f];
+//         const isDocumentType = ["stringValue", "booleanValue", "doubleValue",
+//             "integerValue", "timestampValue", "mapValue", "arrayValue", "nullValue",
+//         ].find((t) => t === key);
+//         if (isDocumentType) {
+//             const item = ["stringValue", "booleanValue", "doubleValue",
+//                 "integerValue", "timestampValue", "nullValue"]
+//                 .find((t) => t === key);
+//             if (item) {
+//                 return value;
+//             } else if ("mapValue" == key) {
+//                 return documentToJson(value.fields || {});
+//             } else if ("arrayValue" == key) {
+//                 const list = value.values;
+//                 return list ? list.map((l) => documentToJson(l)) : [];
+//             }
+//         } else {
+//             result[key] = documentToJson(value);
+//         }
+//     }
+//     return result;
+// }
