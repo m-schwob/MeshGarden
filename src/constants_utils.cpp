948f4e3e3932a44bb7bea0d55bf3a0fbdb593963
m-schwob <menashe54@gmail.com>
2022-07-29 19:28:44 +0300

    fix compiling iss sues

--- src/constants_utils.cpp
+++ src/constants_utils.cpp
@@ -2,21 +2,26 @@
 
 #if defined(ESP8266)
 ADS1X15 ads;
-#endif
 
 float analog_read(uint16_t pin)
 {
     return ads.analog_read(pin);
 }
+#else // ugly plaster fo esp32
+float analog_read(uint16_t pin)
+{
+    return 0.0;
+}
+#endif
+
 // float analogRead(uint16_t pin)
 // {
 //     return ads.analog_read(pin);
 // }
 
 float percentage(float value, float max, float min = 0)
-    {
-        if (min > value || value > max)
-            return -1;
-        return 100*(value - min) / (max - min);
-    }
-
+{
+    if (min > value || value > max)
+        return -1;
+    return 100 * (value - min) / (max - min);
+}
